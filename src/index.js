a32ce5e5d37739f99324e7eef290cdc5fefc69b9
Serhii Yevchishen <magodza06@gmail.com>
2023-04-03 20:23:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 // import './sass/index.scss';
 // import NewsApiService from './js/api-service';
 // import { lightbox } from './js/lightbox';
@@ -40,62 +41,56 @@
 
 // function onLoadMore() {
 //   newsApiService.incrementPage();
-//   fetchGallery();
+//    fetchGallery();
 // }
+
 
 // async function fetchGallery() {
 //   refs.loadMoreBtn.classList.add('is-hidden');
 
-//   const r = await newsApiService.fetchGallery();
-//   const { hits, total } = r;
-//   isShown += hits.length;
+//   // try {
+//     const r = await newsApiService.fetchGallery();
+//     const { hits, total } = r;
 
-//   if (!hits.length) {
-//     Notify.failure(
-//       `Sorry, there are no images matching your search query. Please try again.`
-//     );
-//     refs.loadMoreBtn.classList.add('is-hidden');
-//     return;
-//   }
+//     if (!hits.length) {
+//       Notify.failure(
+//        "Sorry, there are no images matching your search query. Please try again."
+//       );
+//       return;
+//     }
 
-//   onRenderGallery(hits);
-//   isShown += hits.length;
+//     onRenderGallery(hits);
+//     isShown += hits.length;
 
-//   if (isShown < total) {
-//     Notify.success(`Hooray! We found ${total} images !!!`);
-//     refs.loadMoreBtn.classList.remove('is-hidden');
-//   }
-
-//   if (isShown >= total) {
-//     Notify.info("We're sorry, but you've reached the end of search results.");
-//   }
+//     if (isShown < total) {
+//       Notify.success(`Hooray! We found ${total} images !!!`);
+//       refs.loadMoreBtn.classList.remove('is-hidden');
+//       observer.observe(refs.loadMoreBtn);
+//     } else {
+//       Notify.info("We're sorry, but you've reached the end of search results.");
+//     }
+//   } catch (error) {
+//     Notify.failure('Failed to load images. Please try again later.');
+//   // }
 // }
 
 // function onRenderGallery(elements) {
 //   const markup = elements
-//     .map(
-//       ({
-//         webformatURL,
-//         largeImageURL,
-//         tags,
-//         likes,
-//         views,
-//         comments,
-//         downloads,
-//       }) => {
-//         return `<div class="photo-card">
-//     <a href="${largeImageURL}">
-//       <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-//     </a>
-//     <div class="info">
-//       <p class="info-item">
-//         <b>Likes</b>
-//         ${likes}
-//       </p>
-//       <p class="info-item">
-//         <b>Views</b>
-//         ${views}
-//       </p>
+//     .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+//       return `
+//         <div class="photo-card">
+//           <a href="${largeImageURL}">
+//             <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+//           </a>
+//           <div class="info">
+//             <p class="info-item">
+//               <b>Likes</b>
+//               ${likes}
+//             </p>
+//             <p class="info-item">
+//               <b>Views</b>
+//               ${views}
+//  </p>
 //       <p class="info-item">
 //         <b>Comments</b>
 //         ${comments}
@@ -116,7 +111,7 @@ import './sass/index.scss';
 import NewsApiService from './js/api-service';
 import { lightbox } from './js/lightbox';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
+let hits = [];
 const refs = {
   searchForm: document.querySelector('.search-form'),
   galleryContainer: document.querySelector('.gallery'),
@@ -149,54 +144,67 @@ function onSearch(e) {
 
   isShown = 0;
   fetchGallery();
+  onRenderGallery(hits);
+}
+
+function onLoadMore() {
+  newsApiService.incrementPage();
+  fetchGallery();
 }
 
 async function fetchGallery() {
   refs.loadMoreBtn.classList.add('is-hidden');
 
-  try {
-    const r = await newsApiService.fetchGallery();
-    const { hits, total } = r;
+  const r = await newsApiService.fetchGallery();
+  const { hits, total } = r;
+  isShown += hits.length;
 
-    if (!hits.length) {
-      Notify.failure(
-       "Sorry, there are no images matching your search query. Please try again." 
-      );
-      return;
-    }
+  if (!hits.length) {
+    Notify.failure(
+      `Sorry, there are no images matching your search query. Please try again.`
+    );
+    refs.loadMoreBtn.classList.add('is-hidden');
+    return;
+  }
 
-    onRenderGallery(hits);
-    isShown += hits.length;
+  onRenderGallery(hits);
+  isShown += hits.length;
 
-    if (isShown < total) {
-      Notify.success(`Hooray! We found ${total} images !!!`);
-      refs.loadMoreBtn.classList.remove('is-hidden');
-      observer.observe(refs.loadMoreBtn);
-    } else {
-      Notify.info("We're sorry, but you've reached the end of search results.");
-    }
-  } catch (error) {
-    Notify.failure('Failed to load images. Please try again later.');
+  if (isShown < total) {
+    Notify.success(`Hooray! We found ${total} images !!!`);
+    refs.loadMoreBtn.classList.remove('is-hidden');
+  }
+
+  if (isShown >= total) {
+    Notify.info("We're sorry, but you've reached the end of search results.");
   }
 }
 
 function onRenderGallery(elements) {
   const markup = elements
-    .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-      return `
-        <div class="photo-card">
-          <a href="${largeImageURL}">
-            <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-          </a>
-          <div class="info">
-            <p class="info-item">
-              <b>Likes</b>
-              ${likes}
-            </p>
-            <p class="info-item">
-              <b>Views</b>
-              ${views}
- </p>
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+    <a href="${largeImageURL}">
+      <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+    </a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>
+        ${likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b>
+        ${views}
+      </p>
       <p class="info-item">
         <b>Comments</b>
         ${comments}
